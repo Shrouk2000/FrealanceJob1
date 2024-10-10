39f330667753aaf23404f00590010207d423b9f8
shrouk2000 <sa2793@gayoum.edu.eg>
2024-10-10 10:35:48 +0300

    navbar added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar'
+function App(){
+  return (
+    <>
+        <Navbar/>
+        <Header/>
+    </>
+
+  );
+}
+export default App;
